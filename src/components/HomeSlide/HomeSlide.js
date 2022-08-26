ffb14e5e55944b5abd1f8da8b714615e5bc05e8f
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2022-08-26 13:45:10 +0500

    working on slider

--- src/components/HomeSlide/HomeSlide.js
+++ src/components/HomeSlide/HomeSlide.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Autoplay ,EffectFade, Navigation, Pagination } from "swiper";
 
 const HomeSlide = () => {
   return (
-    <div>
+    <div className='homeS'>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={false}
+        pagination={{
+          clickable: true,
+        }}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          // disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Pagination, Navigation ,EffectFade]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
         <div className="homeSlide">
         <img src="https://images.hdqwalls.com/download/1/beautiful-landscape-5k-c1.jpg" alt="" />
         <div className="homeSlide_content">
@@ -14,7 +36,23 @@ const HomeSlide = () => {
             was renewed for a fifth and final season.</h5>  
           </div>
         </div>
-        </div>      
+      </div>      
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="homeSlide">
+        <img src="https://images.hdqwalls.com/download/1/beautiful-landscape-5k-c1.jpg" alt="" />
+        <div className="homeSlide_content">
+          <img src="https://image.tmdb.org/t/p/original/aYKRKkxwkHTP3sGHp8E5tnNHhvy.png" />
+          <div className="homeSlide_text">
+          <h5>Stranger Things is an American science fiction drama television series created by the Duffer
+            Brothers , who also serve as showrunners and are executive producers along with Shawn Levy and
+            Dan Cohen . Its first season was released on Netflix on July 15, 2016. In February 2022, the series
+            was renewed for a fifth and final season.</h5>  
+          </div>
+        </div>
+      </div>      
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
